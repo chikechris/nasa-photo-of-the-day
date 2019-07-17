a59ca1b95b69b5bb791d45a8c2f1f60298590a5c
Chike Chris <cchike2017@gmail.com>
2019-07-17 16:31:28 -0400

    installed axios, obtained API key, created component and js files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./App.css";
 
+/*
+// import api key:
+Get Your API Key
+My API key for cchike2017@gmail.com is:
+iOC4aWWpH7ekr4DMMwv6e6E02WV1eVhhQdbyhUzS
+// With actual student key:
+https://api.nasa.gov/planetary/apod?api_key=iOC4aWWpH7ekr4DMMwv6e6E02WV1eVhhQdbyhUzS
+// With demo key:
+https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14
+// api example.
+*/
+
 function App() {
   return (
     <div className="App">
