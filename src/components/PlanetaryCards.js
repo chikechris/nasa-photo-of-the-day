c099101eeda3899fe70704b1ed3ed4eb77c79012
Chike Chris <cchike2017@gmail.com>
2019-07-18 15:30:02 -0400

    installed semantic ui dependencies. and about to refactor css part of the proor the css part of the project

--- src/components/PlanetaryCards.js
+++ src/components/PlanetaryCards.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Comment } from "semantic-ui-react";
 
 function PlanetaryCards({ date, explanation, hdurl, title, copyright }) {
   return (
-    <div className="planetary-card" key={hdurl}>
-      <img className="planetary-img" src={hdurl} alt="" />
-      <div className="planetary-info">
-        <h2>{title}</h2>
-        <h4>{date}</h4>
-        <p>{explanation}</p>
-        <h4>{copyright}</h4>
-      </div>
-    </div>
+    // <div className="planetary-card" key={hdurl}>
+    //   <img className="planetary-img" src={hdurl} alt="" />
+    //   <div className="planetary-info">
+    //     <h2>{title}</h2>
+    //     <h4>{date}</h4>
+    //     <p>{explanation}</p>
+    //     <h4>{copyright}</h4>
+    //   </div>
+    // </div>
+    <Comment>
+      <Comment.Avatar src={hdurl} width=" 200" />
+      <Comment.Content>
+        <Comment.Text> {title}</Comment.Text>
+        <Comment.Text> {date} </Comment.Text>
+        <Comment.Text> {explanation} </Comment.Text>
+        <Comment.Author> {copyright} </Comment.Author>
+      </Comment.Content>
+    </Comment>
   );
 }
 
