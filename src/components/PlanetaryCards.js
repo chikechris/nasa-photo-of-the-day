63bca341edef3d1730da74c98b4120eb7599dee9
Chike Chris <cchike2017@gmail.com>
2019-07-18 16:40:30 -0400

    installed styled-component. using it to refactor css

--- src/components/PlanetaryCards.js
+++ src/components/PlanetaryCards.js
@@ -1,26 +1,48 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+// import { Comment, CommentGroup, Card } from "semantic-ui-react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  height: 380px;
+  width: 100%;
+  background: rgb(13, 7, 66);
+  display: flex;
+  justify-content: space-around;
+`;
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 50px;
+  margin-top: 20px;
+  margin-left: 10px;
+  margin-right: 20px;
+`;
 
 function PlanetaryCards({ date, explanation, hdurl, title, copyright }) {
   return (
-    // <div className="planetary-card" key={hdurl}>
-    //   <img className="planetary-img" src={hdurl} alt="" />
-    //   <div className="planetary-info">
-    //     <h2>{title}</h2>
-    //     <h4>{date}</h4>
-    //     <p>{explanation}</p>
-    //     <h4>{copyright}</h4>
-    //   </div>
-    // </div>
-    <Comment>
-      <Comment.Avatar src={hdurl} width=" 200" />
-      <Comment.Content>
-        <Comment.Text> {title}</Comment.Text>
-        <Comment.Text> {date} </Comment.Text>
-        <Comment.Text> {explanation} </Comment.Text>
-        <Comment.Author> {copyright} </Comment.Author>
-      </Comment.Content>
-    </Comment>
+    <Div className="planetary-card" key={hdurl}>
+      <Image className="planetary-img" src={hdurl} alt="" />
+      <div className="planetary-info">
+        <h2>{title}</h2>
+        <h4>{date}</h4>
+        <p>{explanation}</p>
+        <h4>{copyright}</h4>
+      </div>
+    </Div>
+
+    // <Card>
+    //   <CommentGroup>
+    //     <Comment>
+    //       <Comment.Avatar src={hdurl} width="200" />
+    //       <Comment.Content>
+    //         <Comment.Text> {title}</Comment.Text>
+    //         <Comment.Metadata> {date} </Comment.Metadata>
+    //         <Comment.Text> {explanation} </Comment.Text>
+    //         <Comment.Author> {copyright} </Comment.Author>
+    //       </Comment.Content>
+    //     </Comment>
+    //   </CommentGroup>
+    // </Card>
   );
 }
 
